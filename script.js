8b8814d499cb99497196d152809c4c7516d7890a
Ollie Khakwani <okhakwani@gmail.com>
2017-06-25 23:21:34 -0400

    added logic to make attribution say anonymous if no attribution

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
+  //Load initial quote
   getQuote();
 
+  //Click handler for New Quote button
   $('.quote--nextQuoteButton').on("click", getQuote);
 });
 
 function getQuote() {
-  //URL to make AJAX request for a new quote
-  //http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en
   $.ajax({
     url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=successCallback",
     dataType: 'jsonp'
   });
 }
 
+//Callback function for JSONP request
 function successCallback(data) {
   console.log(data);
 
+  //Set quote text
   $('.quote--text').html("'" + data.quoteText + "'");
-  $('.quote--author').html(data.quoteAuthor);
+
+  if(data.quoteAuthor) {
+    $('.quote--author').html("–" + data.quoteAuthor);
+  } else {
+    $('.quote--author').html("–Anonymous");
+  }
+
 }
